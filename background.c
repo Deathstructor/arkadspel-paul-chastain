ee49120c1f3ff786a6ad7754c16cd18d80387ae1
Deathstructor <paulchast@gmail.com>
2023-09-12 11:09:31 +0200

    Added background file that contains the code for the background effect

---
+++ background.c
@@ -0,0 +1,36 @@
+#include "raylib.h"
+
+typedef struct
+{
+    Vector2 pos;
+    int speed;
+    bool exist;
+} particle;
+
+const int particle_amount = 500;
+particle particles[particle_amount];
+
+void BackgroundEffect()
+{
+    for (int i = 0; i < particle_amount; i++)
+    {
+        if (!particles[i].exist)
+        {
+            particles[i].pos = (Vector2){GetRandomValue(0, GetScreenWidth()), GetRandomValue(-700, 0)};
+            particles[i].speed = GetRandomValue(2, 5);
+            particles[i].exist = true;
+        }
+
+        if (particles[i].exist)
+        {
+            particles[i].pos.y += particles[i].speed;
+
+            if (particles[i].pos.y > GetScreenHeight())
+            {
+                particles[i].exist = false;
+            }
+            
+            DrawRectangle(particles[i].pos.x, particles[i].pos.y, 1, 1, WHITE);
+        }   
+    }
+}
