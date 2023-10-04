32d0478d6664df3236e4a4a5ddcd4d36fdccb68f
Deathstructor <paulchast@gmail.com>
2023-10-04 10:51:59 +0200

    Added code to call PathOrder function and a switch to load code based on the enemy state

--- arkadspel.c
+++ arkadspel.c
@@ -8,6 +8,7 @@
 #include "enemy.c"
 #include "collision.c"
 #include "path.c"
+#include "formation.c"
 
 int main()
 {
@@ -20,8 +21,7 @@ int main()
 
     LoadPlayer();  // Loads the player texture
     LoadEnemies(); // Loads the enemy texture
-
-    // CreatePath();
+    PathOrder();
 
     // A while loop where everything that needs to be constantly updated is put in.
     while (!WindowShouldClose())
@@ -34,40 +34,32 @@ int main()
         DrawTexture(ship_img, player.pos.x, player.pos.y, WHITE); // Draws the player textures
         PlayerMovement();                                         // Loads the player movements and movement limits
         PlayerShooting();                                         // Loads the projectiles for the player
-        // EnemyMovement();                                          // Loads the enemies movements
-        // EnemyHitbox(); // Loads the enemies hitboxes
+        EnemyMovement();                                          // Loads the enemies movements
+        EnemyHitbox();                                            // Loads the enemies hitboxes
 
-        FollowPath();
-        DrawEnemies(); // Draws the enemy textures
+        switch (0)
+        {
+        case STATE_PATH:
+            FollowPath();
+            break;
 
-        // DrawLineBezierCubic((Vector2){GetScreenWidth() * 0.5 - 50, -50},
-        //                     (Vector2){GetScreenWidth() * 0.5 - 50, 100},
-        //                     (Vector2){GetScreenWidth() * 0.5 - 50, -100},
-        //                     (Vector2){GetScreenWidth() * 0.5 - 50, 150},
-        //                     2,
-        //                     YELLOW);
-        // DrawLineBezierCubic((Vector2){GetScreenWidth() * 0.5 - 50, 100},
-        //                     (Vector2){100, 350},
-        //                     (Vector2){GetScreenWidth() * 0.5 - 50, 200},
-        //                     (Vector2){100, 250},
-        //                     2,
-        //                     YELLOW);
-        // DrawLineBezierCubic((Vector2){100, 350},
-        //                     (Vector2){275, 350},
-        //                     (Vector2){100, 500},
-        //                     (Vector2){275, 500},
-        //                     2,
-        //                     YELLOW);
+        case STATE_FORMATION:
+            break;
 
-        DrawLineBezierCubic(p1_left.curves[0].startPos, p1_left.curves[0].endPos, p1_left.curves[0].startControlPos, p1_left.curves[0].endControlPos, p1_left.curves[0].thick, p1_left.curves[0].color);
-        DrawLineBezierCubic(p1_left.curves[1].startPos, p1_left.curves[1].endPos, p1_left.curves[1].startControlPos, p1_left.curves[1].endControlPos, p1_left.curves[1].thick, p1_left.curves[1].color);
-        DrawLineBezierCubic(p1_left.curves[2].startPos, p1_left.curves[2].endPos, p1_left.curves[2].startControlPos, p1_left.curves[2].endControlPos, p1_left.curves[2].thick, p1_left.curves[2].color);
-        
-        DrawLineBezierCubic(p1_right.curves[0].startPos, p1_right.curves[0].endPos, p1_right.curves[0].startControlPos, p1_right.curves[0].endControlPos, p1_right.curves[0].thick, p1_right.curves[0].color);
-        DrawLineBezierCubic(p1_right.curves[1].startPos, p1_right.curves[1].endPos, p1_right.curves[1].startControlPos, p1_right.curves[1].endControlPos, p1_right.curves[1].thick, p1_right.curves[1].color);
-        DrawLineBezierCubic(p1_right.curves[2].startPos, p1_right.curves[2].endPos, p1_right.curves[2].startControlPos, p1_right.curves[2].endControlPos, p1_right.curves[2].thick, p1_right.curves[2].color);
+        case STATE_DIVE:
+            break;
 
-        DrawText(TextFormat("Completion: %2f", enemies[0].completion), 8, 8, 16, WHITE);
+        default:
+            break;
+        }
+
+        // DrawLineBezierCubic(p1_left.curves[0].startPos, p1_left.curves[0].endPos, p1_left.curves[0].startControlPos, p1_left.curves[0].endControlPos, p1_left.curves[0].thick, p1_left.curves[0].color);
+        // DrawLineBezierCubic(p1_left.curves[1].startPos, p1_left.curves[1].endPos, p1_left.curves[1].startControlPos, p1_left.curves[1].endControlPos, p1_left.curves[1].thick, p1_left.curves[1].color);
+        // DrawLineBezierCubic(p1_left.curves[2].startPos, p1_left.curves[2].endPos, p1_left.curves[2].startControlPos, p1_left.curves[2].endControlPos, p1_left.curves[2].thick, p1_left.curves[2].color);
+
+        // DrawLineBezierCubic(p1_right.curves[0].startPos, p1_right.curves[0].endPos, p1_right.curves[0].startControlPos, p1_right.curves[0].endControlPos, p1_right.curves[0].thick, p1_right.curves[0].color);
+        // DrawLineBezierCubic(p1_right.curves[1].startPos, p1_right.curves[1].endPos, p1_right.curves[1].startControlPos, p1_right.curves[1].endControlPos, p1_right.curves[1].thick, p1_right.curves[1].color);
+        // DrawLineBezierCubic(p1_right.curves[2].startPos, p1_right.curves[2].endPos, p1_right.curves[2].startControlPos, p1_right.curves[2].endControlPos, p1_right.curves[2].thick, p1_right.curves[2].color);
 
         EndDrawing();
 
